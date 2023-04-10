dd3559fbee374d23ee9736f7f43c48d3406b9ca1
MeviusS <71888856+MeviusS@users.noreply.github.com>
2023-04-10 06:34:36 +0300

    Added a settings menu

---
+++ Cura Botanica/Assets/Scripts/MainMenu/SoundManger.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SoundManger : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+    [SerializeField] private TextMeshProUGUI volumeTextUI;
+
+    private void Start()
+    {
+        LoadValues();
+    }
+
+    public void UpdateVolumeSlider(float volume)
+    {
+        int roundedVolume = Convert.ToInt32(volume);
+        string volumeValueText = roundedVolume.ToString();
+        volumeTextUI.SetText(volumeValueText);
+    }
+
+    public void SaveVolumeValue()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+    
+}

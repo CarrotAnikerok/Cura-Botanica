fcddf1336f74e3ad246f07335129294d7ac20d30
аникерок <vika.koreckina@yandex.ru>
2023-04-26 23:39:14 +0300

    Improve more

--- Cura Botanica/Assets/Scripts/MainRoom/Plants/FaseButton.cs
+++ Cura Botanica/Assets/Scripts/MainRoom/Plants/FaseButton.cs
@@ -18,15 +18,37 @@ public class FaseButton : MonoBehaviour, IPointerClickHandler
     public void Awake()
     {
         allPlants = FindObjectsOfType<GamePlant>();
+<<<<<<< Updated upstream
+=======
+        image = GetComponent<Image>();
+
+>>>>>>> Stashed changes
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+<<<<<<< Updated upstream
+=======
+        StartCoroutine(LoadUpdate());
+    }
+
+    IEnumerator LoadUpdate()
+    {
+        transition.StartTransition();
+        yield return new WaitForSeconds(0.2f);
+
+>>>>>>> Stashed changes
         int i = Array.FindIndex(fases, x => x == fase);
         if (i < 2)
         {
             this.fase = fases[i + 1];
+<<<<<<< Updated upstream
         } 
+=======
+            image.sprite = fasesPictures[i + 1];
+            
+        }
+>>>>>>> Stashed changes
         else if (i == 2)
         {
             this.fase = fases[0]; 
@@ -39,6 +61,13 @@ public class FaseButton : MonoBehaviour, IPointerClickHandler
             Debug.Log(plant.plant.name + " в состоянии " + plant.plant.state);
         }
 
+<<<<<<< Updated upstream
         Debug.Log(fase);
+=======
+        transition.EndTransition();
+        yield return new WaitForSeconds(0.5f);
+        transition.ReturnToStart();
+        transition.image.sprite = transition.fases[i];
+>>>>>>> Stashed changes
     }
 }
